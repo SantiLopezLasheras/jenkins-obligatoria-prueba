89a63b8ed32fec5291db4990e9a83adfc3bb6b71
santijll <santijll@example.com>
2025-01-12 18:33:20 +0100

    oldreadme2

--- jenkinsScripts/updateReadme.js
+++ jenkinsScripts/updateReadme.js
@@ -1,5 +1,4 @@
 import { promises as fs } from "fs";
-import path from "path";
 
 async function main() {
   const resultadoJest = process.argv[2];
@@ -11,36 +10,13 @@ async function main() {
         ? "https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg"
         : "https://img.shields.io/badge/tested%20with-Cypress-D32F2F.svg";
 
-    // Obtener la ruta al directorio del script
-    const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
-    // Asegurémonos de que estamos accediendo correctamente al archivo OldREADME.md
-    const ruta_OldREADME = path.join(__dirname, "..", "OldREADME.md");
-
-    // Depuración: Verifica la ruta que se está construyendo
-    console.log("Ruta de OldREADME.md:", ruta_OldREADME);
-
-    // Verificar si el archivo OldREADME.md existe en la ruta esperada
-    try {
-      await fs.access(ruta_OldREADME);
-      console.log("El archivo OldREADME.md existe");
-    } catch (error) {
-      console.log("El archivo OldREADME.md NO existe en la ruta especificada");
-      process.exit(1); // Termina con error si el archivo no existe
-    }
-
-    // Leemos el archivo OldREADME.md
-    const old_readme = await fs.readFile(ruta_OldREADME, "utf8");
-
-    // Generar el nuevo contenido para README.md
+    const old_readme = await fs.readFile("../OldReadme.md", "utf8");
     const new_readme = `<img src="${badge}" />` + "\n" + old_readme;
 
-    // Escribir el nuevo contenido en README.md
-    await fs.writeFile(path.join(__dirname, "..", "README.md"), new_readme);
+    await fs.writeFile("./README.md", new_readme);
     process.exit(0);
   } catch (error) {
-    console.log("Error:", error);
-    process.exit(1);
+    console.log(error);
   }
 }
 
