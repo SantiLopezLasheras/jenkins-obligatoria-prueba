bd42286c8b28e2676242d4330bd5aca45854ab54
santijll <santijll@example.com>
2025-01-12 18:51:59 +0100

    prueba

--- jenkinsScripts/pushChanges.js
+++ jenkinsScripts/pushChanges.js
@@ -1,18 +1,29 @@
-import { execSync } from "child_process";
+const { execSync } = require("child_process");
 
 // Obtenemos el executor y el motivo como parámetros de la pipeline
 const executor = process.argv[2];
-const motiu = process.argv[3];
+const motivo = process.argv[3];
 
 // Cambiar al directorio raíz del repositorio
-execSync('git config --global user.name "SantiLopezLasheras"');
+execSync('git config --global user.name "SantiLopezLasheras');
 execSync('git config --global user.email "sanloplas@alu.edu.gva.es"');
+
+// Aseguramos que las ramas remotas estén actualizadas
+execSync("git fetch origin");
+
+// Verificamos si estamos en la rama correcta (main) y cambiamos si es necesario
+try {
+  execSync("git checkout main");
+  console.log("Cambiado a la rama main");
+} catch (e) {
+  console.log("Ya estamos en la rama main: " + e);
+}
 
 // Añadir cambios en el archivo README
 execSync("git add README.md");
 
 // Hacer commit con el mensaje solicitado
-const commitMessage = `Pipeline executada per ${executor}. Motiu: ${motiu}`;
+const commitMessage = `Pipeline executada per ${executor}. Motiu: ${motivo}`;
 execSync(`git commit -m "${commitMessage}"`);
 
 // Hacer push de los cambios al repositorio
