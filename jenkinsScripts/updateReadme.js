7fc1143613240b24acc8e927aabbaa987906ebc1
santijll <santijll@example.com>
2025-01-12 18:10:06 +0100

    update5

--- jenkinsScripts/updateReadme.js
+++ jenkinsScripts/updateReadme.js
@@ -1,64 +1,23 @@
-import { readFile, writeFile } from "fs";
-import { join } from "path";
+import { promises as fs } from "fs";
 
-console.log(`HOLA DESDE UPDATEREADME: ${process.argv[2]}`);
+async function main() {
+  const resultadoJest = process.argv[2];
+  console.log(`HOLA DESDE UPDATEREADME: ${resultadoJest}`);
 
-// Path del README
-// eslint-disable-next-line no-undef
-const readmePath = join(__dirname, "..", "README.md");
+  try {
+    const badge =
+      resultadoJest === "success"
+        ? "https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg"
+        : "https://img.shields.io/badge/tested%20with-Cypress-D32F2F.svg";
 
-// URL del badge
-const badgeSuccess =
-  "https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg";
-const badgeFailure =
-  "https://img.shields.io/badge/tested%20with-Cypress-D32F2F.svg";
+    const old_readme = await fs.readFile("./OldREADME.md", "utf8");
+    const new_readme = `<img src="${badge}" />` + "\n" + old_readme;
 
-// Función para actualizar el README
-const updateReadme = (status) => {
-  // Lee el archivo README.md
-  readFile(readmePath, "utf8", (err, data) => {
-    if (err) {
-      console.error("Error leyendo el README.md:", err);
-      process.exit(1);
-    }
-
-    // Elige el badge según el resultado de las pruebas
-    const badge = status === "success" ? badgeSuccess : badgeFailure;
-
-    // Regex para encontrar la línea donde se inserta el badge
-    const badgeRegex =
-      /!\[Test Status\]\(https:\/\/img\.shields\.io\/badge\/tested%20with-Cypress-.*\.svg\)/;
-
-    // Si el badge existe, lo reemplazamos; si no, lo añadimos al principio
-    if (badgeRegex.test(data)) {
-      const updatedData = data.replace(badgeRegex, `![Test Status](${badge})`);
-      writeUpdatedReadme(updatedData);
-    } else {
-      const updatedData = `![Test Status](${badge})\n\n` + data;
-      writeUpdatedReadme(updatedData);
-    }
-  });
-};
-
-// Función para escribir el archivo README actualizado
-const writeUpdatedReadme = (updatedData) => {
-  writeFile(readmePath, updatedData, "utf8", (err) => {
-    if (err) {
-      console.error("Error escribiendo el README.md:", err);
-      process.exit(1);
-    }
-    console.log("README.md actualizado con éxito!");
-  });
-};
-
-// Obtener el estado de las pruebas como argumento (success o failure)
-const status = process.argv[2];
-if (!status || (status !== "success" && status !== "failure")) {
-  console.error(
-    'Por favor, proporciona un estado válido ("success" o "failure")'
-  );
-  process.exit(1);
+    await fs.writeFile("./README.md", new_readme);
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// Llamar a la función para actualizar el README
-updateReadme(status);
+main();
